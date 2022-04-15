e752d29f4622a860346e422c9f6e99ea6f1b3e75
XNycX <nycx2009@hotmail.com>
2022-04-15 17:48:46 +0200

    trying to fix heroku2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('userName');
-            $table->string('email')->unique();
+            $table->string('email',30)->unique();
             $table->string('steamUserName');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
